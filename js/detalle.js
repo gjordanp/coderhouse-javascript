f23958c48115f5adee2cafa3d176250fc1bd88c5
Gabriel Jordan <gjordanp@gmail.com>
2022-12-11 11:27:01 -0300

    graph2

--- js/detalle.js
+++ js/detalle.js
@@ -1,16 +1,31 @@
+// Real Viewport Height---------------------------------------------------------------------------
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+// Real Viewport Height---------------------------------------------------------------------------
+
 const ctx = document.getElementById('myChart1');
 
   new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: ['Muros', 'Perimetral', 'Vigas', 'Losas', 'Escalera'],
       datasets: [{
-        label: '# of Votes',
+        label: 'Kg de Acero',
         data: [12, 19, 3, 5, 2, 3],
         borderWidth: 1
       }]
     },
     options: {
+        responsive: true,
+        maintainAspectRatio: false,
       scales: {
         y: {
           beginAtZero: true
@@ -19,6 +34,7 @@ const ctx = document.getElementById('myChart1');
     }
   });
 
+  
   const ctx2 = document.getElementById('myChart2');
 
   new Chart(ctx2, {
@@ -32,6 +48,8 @@ const ctx = document.getElementById('myChart1');
       }]
     },
     options: {
+        responsive: true,
+        maintainAspectRatio: false,
       scales: {
         y: {
           beginAtZero: true
@@ -41,6 +59,9 @@ const ctx = document.getElementById('myChart1');
   });
 
   const ctx3 = document.getElementById('myChart3');
+  const ctx4 = document.getElementById('myChart4');
+  const ctx5 = document.getElementById('myChart5');
+  const ctx6 = document.getElementById('myChart6');
 
   const data = {
     labels: [
@@ -60,7 +81,60 @@ const ctx = document.getElementById('myChart1');
     }]
   };
 
+  const data2 = {
+    labels: [
+      'Armadura',
+      'Punta',
+      'Confinamiento'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)'
+      ],
+      hoverOffset: 4
+    }]
+  };
+
   new Chart(ctx3, {
     type: 'doughnut',
     data: data,
+    options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+      
+  });
+
+  new Chart(ctx4, {
+    type: 'doughnut',
+    data: data2,
+    options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+      
+  });
+
+  new Chart(ctx5, {
+    type: 'doughnut',
+    data: data2,
+    options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+      
+  });
+
+  new Chart(ctx6, {
+    type: 'doughnut',
+    data: data2,
+    options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+      
   });
